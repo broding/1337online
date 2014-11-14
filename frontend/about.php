6cd23a1b1663be9c0574df492cff015aa712c215
Gunnaway <stevengunneweg@gmail.com>
2014-11-14 21:14:22 +0100

    added api folder

--- frontend/about.php
+++ frontend/about.php
@@ -8,6 +8,6 @@
 		<a href="/..">home</a>
 		<h1>About</h1>
 		<p>the goal is to click the button when the time is 13:37. The closer you get to that exact time the better your score is.</p>
-		<p>This page is currently still under development.</p>
+		<p>This website is currently still under development.</p>
 	</body>
 </html>
